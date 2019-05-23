ae208ec6ad0bc182389da8facffc8d24d62bc764
max delzer <emma.delzer@gmail.com>
2019-05-22 23:32:10 -0700

    Created variables, button, and startGame(){

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,9 +10,54 @@
 // seconds
 // On FINAL SCREEN show player the number of correct and incorrect answers, AND option to restart game
 
-setTimeout(fiveSeconds, 1000 * 5);
-
 function fiveSeconds() {
     $("#time-left").append("<h2>This is a test.</h2>");
     console.log("Hello World!");
   }
+
+//Variables
+var time = 30;
+var score = 0;
+var questions = 
+{
+    q1: ["In what year did The Transformers cartoon premier?"],
+     answers1:
+            [{
+            'a':'b',
+            'c':'d',
+            'e':'f',
+            'g':'h',
+            }]
+};
+
+var correctAnswers;
+var wrongAnswers;[]
+var intervalId;
+
+/*Functions */
+//test
+console.log(questions.answers1[0].c);
+
+function startGame(){
+} function gamePlay(){
+    $('.startBtn').on('click', function(){
+        $('button').remove('.startBtn');
+        quest1(); 
+    });
+}
+function quest1()
+{
+    var newDiv = $('<div class="quest q1">');
+    newDiv.html(questions.q1);
+
+    $('.btnContainer').apppend(newDiv);
+    intervalID = setInterval(timeCount,1000);
+}
+function timeCount(){
+    time--;
+    //test
+    console.log(time);
+    //clears and stops time
+    if(time === 0)
+    {}
+}
