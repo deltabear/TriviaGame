ebe79b800846f387ab8f7318cd6e04a924d86a1a
max delzer <emma.delzer@gmail.com>
2019-05-24 20:48:01 -0700

    Removed testing console, added question function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,42 +9,55 @@
 // IF player chooses incorrect answer, inform player that answer was wrong AND display correct answer for several
 // seconds
 // On FINAL SCREEN show player the number of correct and incorrect answers, AND option to restart game
+$(document).ready(function(){
 
-function fiveSeconds() {
-    $("time-left").append("<h2>This is a test.</h2>");
-    console.log("Hello World!");
-  }
+    $('button').click(function(){
+    trivia.startGame});
+    })
 
-$('button').click(function(){
-    console.log("This is a test!")
-});
-//Variables
-var time = 30;
-var score = 0;
-var questions = 
-{
-    q1: ["In what year did The Transformers cartoon premier?"],
-     answers1:
-            [{
-            a:'b',
-            c:'d',
-            e:'f',
-            g:'h',
-            }],
-    q2: ["Who provided the voice for Arcee, the first female Transformer?"],
-    answers2:
-            [{
-            a: 'b2',
-            c: 'd2',
-            e:'f2',
-            g:'h2',
-            }]
-};
+    //Variables
+    var score = 0;
+    var time = 20;
 
-var correctAnswers;
-var wrongAnswers;[]
-var intervalId;
+    //questions and answers data
+    let questions = 
+    {
+        q1: ["In what year did The Transformers cartoon premier?"],
+        choices1:
+                [{
+                A: '1989',
+                B: '1984',
+                C: '1980',
+                D: '1985',
+                answer: 'b',
+                }],
+        q2: ["Who provided the voice for Arcee, the first female Transformer in the 1986 movie?"],
+        choices2:
+                [{
+                A: 'Tress MacNeille',
+                B: 'June Foray',
+                C: 'Susan Blu',
+                D: 'Lucille Bliss',
+                answer: 'c',
+                }], 
+    };
+
+    let lastQuestionIndex = questions.length - 1;
+    let runningQuestionIndex = 0;
+
+    function renderQuestion(){
+        let q = questions[runningQuestionIndex];
+        question.innerHTML = "<p>" + q.question + "</p>";
+        choiceA.innerHTML = q1.choices1.a;
+        choiceB.innerHTML = q1.choices1.b;
+        choiceC.innerHTML = q1.choices1.c;
+        choiceD.innerHTML = q1.choices1.d;
+    }
+
+    var correctAnswers;
+    var wrongAnswers;[]
+    var intervalId;
 
 /*Functions */
 //test
-console.log(questions.q2)
+//console.log(questions.q2)
